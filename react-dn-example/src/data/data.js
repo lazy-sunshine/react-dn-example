46609c0433e08b0a8203e3391b56d20147701760
lazy-sunshine <kirti7756@gmail.com>
2016-10-21 22:05:46 +0530

    minor modification

---
+++ react-dn-example/src/data/data.js
@@ -0,0 +1,43 @@
+export default [
+  {
+    id: 75738,
+    desc: 'Join Netflix and HTC Creative Labs at Design on the Rocks | Thursday Oct 27 | @ The Piranha Shop,Seattle WA ',
+    comment: 3,
+    time: '5 hours ago',
+    image: 'Michaela Wagone UI Motion Designer,Netflix',
+    upvote:13,
+  },
+  {
+    id: 75708,
+  desc: ' Should your team be using Figma? ',
+    comment: 3,
+    time: '12 hours ago',
+    image: 'Chris Meyers< Lead Designer @ ProlificInteractive',
+    upvote:10,
+  },
+   {
+    id: 75711,
+  desc: 'Site Design: Herman Miller – Aeron Remastered ',
+    comment: 3,
+    time: '5 hours ago',
+    image: 'Michaela Wagoner</a>, UI Motion Designer,Netflix',
+    upvote:3,
+  },
+   {
+    id: 75712,
+  desc: 'Join Netflix and HTC Creative Labs at Design on the Rocks | Thursday Oct 27 | @ The Piranha Shop,Seattle WA ',
+    comment: 3,
+    time: '5 hours ago',
+    image: 'Michaela Wagoner</a>, UI Motion Designer,Netflix',
+    upvote:53,
+  },
+   {
+    id: 75713,
+  desc: 'Join Netflix and HTC Creative Labs at Design on the Rocks | Thursday Oct 27 | @ The Piranha Shop,Seattle WA ',
+    comment: 3,
+    time: '5 hours ago',
+    image: 'Michaela Wagoner</a>, UI Motion Designer,Netflix',
+    upvote:23,
+  },
+  
+]
