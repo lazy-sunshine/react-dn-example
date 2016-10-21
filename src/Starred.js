29e2e34fabdd43883759077fe775f1a584404b46
kchat1 <kchaturvedi2@sapient.com>
2016-10-21 17:35:02 +0530

    added styling to the star element

--- src/Starred.js
+++ src/Starred.js
@@ -7,16 +7,17 @@ class Starred extends React.Component{
     ;
   }
 addToStar(){
-    console.log(this.props);
+    this.refs.star.setAttribute('fill','yellow');
+    this.refs.star.setAttribute('stroke','black')
     this.props.starArticle(this.props.id);
 }
 
 
     render(){
          return(
-            <div className="list-story-grouper list-story-star-grouper">
+            <div className="list-story-grouper list-story-star-grouper" >
                 <a href="#" className="story-star-button" onClick={this.addToStar}>
-                    <svg height="15" id="svg-star" role='img version="1.1"' viewBox="0 0 15 15" width="15">
+                    <svg ref="star" height="15" id="svg-star" role='img version="1.1"' viewBox="0 0 15 15" width="15">
                         <path d="m7.5 11.453l4.635 2.797-1.23-5.273 4.095-3.547-5.393-.457-2.108-4.973-2.108 4.973-5.393.457 4.095 3.547-1.23 5.273 4.635-2.797"/>
                     </svg>
                 </a>
